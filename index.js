ea7b16db75a03848ea3b06b2cb72ee256b841a6c
Replit user <>
2023-06-08 01:12:30 +0000

    Creating a basic setup for the Educarry website

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
-
+const path = require('path'); 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello Express app!')
-});
+// Set 'views' directory and template engine
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
+
+// Include the routes
+const indexRouter = require('./src/routes/index');
+app.use('/', indexRouter);
 
 app.listen(3000, () => {
-  console.log('server started');
+  console.log('Server started on port 3000');
 });
