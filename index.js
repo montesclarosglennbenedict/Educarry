8cb0dd67c0f6c8c7b00c98e43be7bc3b9b705d63
Replit user <>
2023-06-08 17:24:54 +0000

    Got the CSS file to load yesterday

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express');
-const path = require('path'); 
+const path = require('path');
 const app = express();
 
 // Set 'views' directory and template engine
 app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'ejs');
 
+// Serve static files from the 'public' directory
+app.use(express.static('src/public'));
+
+// ...
+
 // Include the routes
 const indexRouter = require('./src/routes/index');
 app.use('/', indexRouter);
+
+// Error handler middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send('Internal Server Error');
+});
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
